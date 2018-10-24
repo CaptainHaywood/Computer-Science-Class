f55d3524dcac9394596d08e7256050af8ca7aaab
James Haywood <jameshaywood823@gmail.com>
2018-10-24 13:35:21 -0400

    10-24-2018

---
+++ Unit 3/03.03.01.py
@@ -0,0 +1,27 @@
+import math
+
+# -- Function Definitions
+def area_of_circle(radius):
+    area = math.pi * radius * radius
+    return area
+
+# -- Main Section
+radiusA = float(input("Radius? "))
+radiusB = float(input("Radius? "))
+radiusC = float(input("Radius? "))
+radiusD = float(input("Radius? "))
+radiusE = float(input("Radius? "))
+
+totalArea = 0
+
+area = area_of_circle(radiusA)
+totalArea = totalArea +  area
+area = area_of_circle(radiusB)
+totalArea = totalArea +  area
+area = area_of_circle(radiusC)
+totalArea = totalArea +  area
+area = area_of_circle(radiusD)
+totalArea = totalArea +  area
+area = area_of_circle(radiusE)
+totalArea = totalArea +  area
+print(area)
