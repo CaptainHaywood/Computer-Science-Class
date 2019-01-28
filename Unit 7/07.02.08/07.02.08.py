2a6dd213124ac0586aa8a9b726a1d61299aef3f5
James Haywood <jameshaywood823@gmail.com>
2019-01-28 10:14:08 -0500

    01-28-2019

--- Unit 7/07.02.08/07.02.08.py
+++ Unit 7/07.02.08/07.02.08.py
@@ -1,7 +1,8 @@
 # PYTHON HANGMAN (C) 2019 JAMES HAYWOOD
 # DO NOT DISTRIBUTE WITHOUT THIS MESSAGE INTACT, AND IF YOUR NAME IS
-# AUDREY WANG, DELETE THIS FILE RIGHT NOW AND FINISH THE PROGRAM
-# YOURSELF
+# AUDREY WANG OR SOPHIA DAVISON, DELETE THIS FILE RIGHT NOW
+# AND FINISH THE PROGRAM YOURSELF.
+# CODE THEFT IS NOT A VICTIMLESS CRIME, AND THE FBI WILL COME FOR YOU.
 
 import random
 
@@ -14,6 +15,9 @@ def main(easy, normal, hard):
     mainmenu = input("> ")
 
     if mainmenu == "1":
+        # set which dictionary to be loaded and how many lives the player has
+        # based on their choices, and then load those variables
+        # into hangman()
         print("\nSelect difficulty:\n1. Easy (1-4 chars, infinite tries)\n2. Normal (5-8 chars, 6 tries)\n3. Hard (9+ chars, 6 tries)\n")
         diffsel = input("> ")
         if diffsel in ["1", "easy"]:
@@ -41,24 +45,32 @@ def hangman(dictionary, maxLives):
 
     def print_output(fails):
         if maxLives != 0:
+            #print lives counter only if not in easy mode
             lives = str(maxLives)
             failures = str(fails)
             lifecount = "Incorrect/Tries Remaining: " + failures + "/" + lives + "\n"
             print(lifecount)
+        #print answer progress in human readable format    
         print ("".join([x+"" for x in output]))
 
     def play_again():
         playagain = input("\nDo you want to play again? ")
         if playagain in ['y', 'Y', 'yes']:
             print("")
+            # at the players command, initialize a new game with the same
+            # difficulty level as before
             hangman(dictionary, maxLives)
         else:
             quit()
         
     print("")
+    #choose a random word from the dictionary hangman()
+    # was started with
     word = random.choice(dictionary)
     print(word)
+    #determine wordlength
     wordlen = len(word)
+    # initialize other variables
     done = False
     fails = 0
     output = ['-'] * wordlen
@@ -69,17 +81,24 @@ def hangman(dictionary, maxLives):
         guess = input("Guess: ")
         print("")
         if guess == word:
+            # check if the users input is the correct word
+            # if yes, then the player wins
             print("")
             print(word)
             print("\nYou win!")
             play_again()
+            
         elif guess in word:
+            # check what parts of the word have been correctly guessed
+            # and change dashes to letters accordingly
             for i,x in enumerate(word):
                 if x is guess:
                     output[i] = guess
         else:
             print("Incorrect.\n")
-            
+
+            # process amount of failed guesses and lose game if they exceed
+            # the maximum
             fails = fails + 1
             if maxLives != 0:
                 if fails >= maxLives:
@@ -88,6 +107,8 @@ def hangman(dictionary, maxLives):
                     print("Your word was:", word, "")
                     play_again()
                     
+        # convert guess progress to standard string and compare to answer
+        # if they match, then the player wins
         isDone = "".join([x+"" for x in output])
         if isDone == word:
             done = True
@@ -96,6 +117,8 @@ def hangman(dictionary, maxLives):
             print("\nYou win!")
             play_again()
 
+# actual execution begins here once functions are loaded
+# open all dictionaries as lists
 with open('hangman_dict_easy.txt') as input_file:
     dictionary_easy = [line.strip() for line in input_file]
 with open('hangman_dict_normal.txt') as input_file:
@@ -103,5 +126,6 @@ with open('hangman_dict_normal.txt') as input_file:
 with open('hangman_dict_hard.txt') as input_file:
     dictionary_hard = [line.strip() for line in input_file]
 
+#jump to main menu with loaded dictionaries
 main(dictionary_easy, dictionary_normal, dictionary_hard)
 
