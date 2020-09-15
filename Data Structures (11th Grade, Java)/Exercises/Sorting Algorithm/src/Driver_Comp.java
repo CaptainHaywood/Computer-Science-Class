90b11f7ea895fb047ff048bd0cd5f6e0fa586085
James <jameshaywood823@gmail.com>
2020-09-15 14:30:34 -0400

    09-15-2020

--- Data Structures (11th Grade, Java)/Exercises/Sorting Algorithm/src/Driver_Comp.java
+++ Data Structures (11th Grade, Java)/Exercises/Sorting Algorithm/src/Driver_Comp.java
@@ -13,8 +13,36 @@ public class Driver_Comp {
         System.out.println(myList);
         sort(myList);
         System.out.println(myList);
-    }
-    public static void sort(ArrayList<Integer> theList){
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("Sophia", "Davison", 3.8));
+        students.add(new Student("Jack", "Miller", 0.0));
+        sort(students);
 
+        String s1 = "apple";
+        String s2 = "cat";
+
+        if(s1.compareTo(s2) < 0){
+            System.out.println("1 lt 2");
+        }
+        else if(s1.compareTo(s2) > 0){
+            System.out.println("1 gt 2");
+        }
+        else{
+            System.out.println("=");
+        }
+    }
+    public static void sort(ArrayList<? extends Comparable> theList){
+        boolean sorted = false;
+        while(!sorted){
+            sorted = true;
+            for(int i = 0; i < theList.size() - 1; i++){
+                if(theList.get(i).getGPA() > theList.get(i+1).getGPA()){
+                    Object hold = theList.get(i);
+                    theList.set(i, theList.get(i+1));
+                    theList.set(i+1, hold);
+                    sorted = false;
+                }
+            }
+        }
     }
 }
